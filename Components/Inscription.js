c2f883c6077b348316051e48be51ac789c9f0f05
Alhuin <janin.reynaud.julien@gmail.com>
2019-06-26 10:25:50 +0200

    Inscription page update

--- Components/Inscription.js
+++ Components/Inscription.js
@@ -1,7 +1,25 @@
 import React from 'react'
-import {Image, StyleSheet, View, Text, TextInput} from "react-native";
+import {Image, StyleSheet, View, Text, TextInput, TouchableOpacity} from "react-native";
+import SocialButton from "rtg-rn-social-buttons";
 
 class Inscription extends React.Component {
+    state = {
+        email: '',
+        password: ''
+    };
+
+    _updateEmail = (text) => {
+        this.setState({ email: text })
+    };
+
+    _updatePassword = (text) => {
+        this.setState({ password: text })
+    };
+
+    _login = () => {
+        alert('email: ' + this.state.email + ' password: ' + this.state.password)
+    };
+
     render(){
         return (
             <View style = {styles.mainContainer}>
@@ -13,10 +31,30 @@ class Inscription extends React.Component {
                     <Text style = {styles.title}>MusicRoom</Text>
                 </View>
                 <View style={styles.loginContainer}>
-                    <TextInput placeholder = "email"/>
-                    <TextInput placeholder = "password"  secureTextEntry={true}/>
+                    <Text>Sign in with email</Text>
+                    <TextInput style={styles.input}
+                               placeholder = "email"
+                               underlineColorAndroid = "transparent"
+                               placeholderTextColor = "#9a73ef"
+                               autoCapitalize = "none"
+                               onChangeText = {this._updateEmail} />
+                    <TextInput style={styles.input}
+                               placeholder = "password"
+                               secureTextEntry={true}
+                               underlineColorAndroid = "transparent"
+                               placeholderTextColor = "#9a73ef"
+                               autoCapitalize = "none"
+                               onChangeText = {this._updatePassword}/>
+                    <TouchableOpacity
+                        style = {styles.submitButton}
+                        onPress = {() => this._login()}>
+                        <Text style = {styles.submitButtonText}> Submit </Text>
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.oAuthContainer}>
+                    <Text>or</Text>
+                    <SocialButton type='facebook' text='Sign In With Facebook' opacity={0.5} height={40} width={220} action={()=>{alert("facebook login")}}/>
+                    <SocialButton type='google' text='Sign In With Google' opacity={0.5} height={40} width={220} action={()=>{alert("google login")}}/>
                 </View>
             </View>
     )}
@@ -25,23 +63,24 @@ class Inscription extends React.Component {
 const styles = StyleSheet.create({
 
     mainContainer:{
-        paddingTop:75,
+        paddingTop:150,
         paddingBottom:200,
         flex:1,
         alignItems: "center",
         justifyContent: "space-around",
-        borderWidth: 1,
-        borderColor: "red",
-        borderStyle: "solid"
+        // borderWidth: 1,
+        // borderColor: "red",
+        // borderStyle: "solid"
     },
     titleContainer:{
+        marginBottom: "10%",
         width: "100%",
         flexDirection:"row",
         justifyContent: "space-around",
         alignItems: "center",
-        borderWidth: 1,
-        borderColor: "black",
-        borderStyle: "solid"
+        // borderWidth: 1,
+        // borderColor: "black",
+        // borderStyle: "solid"
     },
     title:{
         fontSize: 20,
@@ -52,20 +91,38 @@ const styles = StyleSheet.create({
         height: 100,
     },
     loginContainer: {
-        padding: 50,
+        marginTop: "50%",
         alignItems: "center",
         width: "100%",
+        // borderWidth: 1,
+        // borderColor: "blue",
+        // borderStyle: "solid"
+    },
+    input: {
+        paddingLeft:"10%",
+        margin: 15,
+        height: 40,
+        borderColor: 'grey',
         borderWidth: 1,
-        borderColor: "blue",
-        borderStyle: "solid"
+        width:"80%"
     },
     oAuthContainer:{
+        marginTop: "50%",
+        // justifyContent: "center",
+        alignItems: "center",
         width: "100%",
-        padding: 50,
-        height: 100,
-        borderWidth: 1,
-        borderColor: "orange",
-        borderStyle: "solid"
+        // borderWidth: 1,
+        // borderColor: "orange",
+        // borderStyle: "solid"
+    },
+    submitButton: {
+        backgroundColor: '#7a42f4',
+        padding: 10,
+        margin: 15,
+        height: 40,
+    },
+    submitButtonText:{
+        color: 'white'
     }
 });
 
