7adf591e27b7c938418fd6d70e92e14a9466b067
magaspar <magaspar@student.le-101.fr>
2019-09-13 16:58:54 +0200

    RangeDatePicker

--- Front/API/BackApi.js
+++ Front/API/BackApi.js
@@ -1,6 +1,6 @@
 import CustomError from './errorHandler';
 
-const server = 'http://10.4.5.6:3000/api';
+const server = 'http://10.4.2.6:3000/api';
 
 function login(userName, password) {
   return new Promise((resolve, reject) => {
