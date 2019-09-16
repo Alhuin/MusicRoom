673d490d732a76da703796425c99bf9941d4a1f3
magaspar <magaspar@student.le-101.fr>
2019-09-16 15:02:43 +0200

    add asset pour home

--- Front/src/components/AddFloatingButton.js
+++ Front/src/components/AddFloatingButton.js
@@ -15,6 +15,8 @@ export default class AddFloatingButton extends React.Component {
       image = <Image source={require('../assets/images/addPlaylist.png')} style={{ height: 32, width: 32, transform: [{ translateX: +3 }, { translateY: +2 }] }} />;
     } else if (icon === 'addMusic') {
       image = <Image source={require('../assets/images/addMusic.png')} style={{ height: 38, width: 38, transform: [{ translateY: +1 }, { translateX: +1 }] }} />;
+    } else if (icon === 'joinPrivateRoom') {
+      image = <Image source={require('../assets/images/glasse.png')} style={{ height: 38, width: 38, transform: [{ translateY: +1 }, { translateX: +1 }] }} />;
     } else {
       image = <Text style={styles.fabIcon}>+</Text>;
     }
